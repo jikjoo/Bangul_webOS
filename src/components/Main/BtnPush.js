0d30583780dc54eb5d03adb3ce6c7b3eed03f1e5
jikjoo <Jikjoo@JIKTOP>
2020-08-01 23:50:40 +0900

    주석 달기

--- src/components/Main/BtnPush.js
+++ src/components/Main/BtnPush.js
@@ -7,23 +7,29 @@ import './Main.less'
 import { sendCheckConnect } from '../../actions';
 import { connect } from 'react-redux';
 
+// Class형 component 
 class BtnPush extends React.Component {
+    //생성자
     constructor(props) {
         super(props)
+        // 상태 : react에서 변수를 저장하는 곳
         this.state = { alarmOn: false }
+        // onPush 함수를 component에 종속시키기
         this.onPush = this.onPush.bind(this);
     }
+    //BtnPush가 rendering 되고 난 직후
     componentDidMount() {
         const { push, onCheck } = this.props;
         onCheck(push);
     }
     onPush = (e) => {
-        const { push, check, history,onCheck } = this.props;
+        const { push, check, history, onCheck } = this.props;
         onCheck(push);
         if (push === 'home' || push === 'kennel' && !check[push].isOn)
             this.setState({ alarmOn: true });
-        else
-            history.push(push)
+        // 장치들과 연결이 안됐을 때, 화면으로 넘어갈 수 있을지 말지
+        //else
+        history.push(push)
     }
     render() {
         const { push, children, check, onCheck, ...rest } = this.props;
@@ -40,7 +46,11 @@ class BtnPush extends React.Component {
         )
     }
 }
-/* 
+/*
+Container 부분
+redux랑 연결 시키기 위한 코드. 
+
+redux-store 내의 state 안에 check의 구조
 check : {
     home : {
         isOn : false,
@@ -55,13 +65,13 @@ check : {
 const mapStateToProps = ({ check }) => ({
     check
 });
-
+// 장치의 연결을 확인하는 action과 onCheck 함수 연결하기
 const mapDispatchToProps = (dispatch) => {
     return {
         onCheck: (target) => dispatch(sendCheckConnect(target))
     };
 };
-
+// withRouter는 this.props.history 사용할 수 있도록 하기 : 다른 화면으로 넘어가도록
 const BtnPushContainer = connect(mapStateToProps, mapDispatchToProps)(withRouter(BtnPush));
 
 export default BtnPushContainer;
