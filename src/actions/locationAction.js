f6233dce14583edc04fdb495ebeabb329373d997
jikjoo <Jikjoo@JIKTOP>
2020-08-15 03:01:20 +0900

    location check once

--- src/actions/locationAction.js
+++ src/actions/locationAction.js
@@ -11,13 +11,16 @@ export const changeLoadMap = (isLoaded) => {
     }
 }
 
-export const loadNaverMap = params => dispatch => {
-    const script = document.createElement('script');
-    script.type = "text/javascript"
-    script.async = true;
-    script.src = "https://openapi.map.naver.com/openapi/v3/maps.js?ncpClientId=mda3e6eja3";
-    document.head.appendChild(script);
-    script.onload = () => {
-        return dispatch(checkConnect({target:'location',isOn:true}))
+export const loadNaverMap = params => (dispatch, getState) => {
+    const { check } = getState();
+    if (!check.location.isOn) {
+        const script = document.createElement('script');
+        script.type = "text/javascript"
+        script.async = true;
+        script.src = "https://openapi.map.naver.com/openapi/v3/maps.js?ncpClientId=mda3e6eja3";
+        document.head.appendChild(script);
+        script.onload = () => {
+            return dispatch(checkConnect({ target: 'location', isOn: true }))
+        }
     }
 }
